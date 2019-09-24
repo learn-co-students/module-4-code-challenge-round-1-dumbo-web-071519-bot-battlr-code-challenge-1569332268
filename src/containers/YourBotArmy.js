862ee92121526d0517f7f68b5d549a126ccc6769
jason <jasonedlewis@gmail.com>
2019-09-24 11:20:29 -0400

    Jason Lewis

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,19 +4,25 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
-  render(){
+displayArmy=()=>{
+
+	  return <BotCard bot={this.props.yourBots}/>
+	}
+	
+  
+  render() {
+    console.log("Army Props:", this.props);
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.displayArmy()}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
-};
+}
 
 export default YourBotArmy;
