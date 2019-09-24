3eb2b9dfa5925a908fcaaee2fcf571d027ccd65e
github username <github email address>
2019-09-24 11:17:45 -0400

    Halfway Done

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,37 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = {
+    allBotsData: [],
+    botArmy: [], //should I move this to YourBotArmy?
+    toAddBot: []
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(response => response.json())
+    .then(botsData => this.setState({
+      allBotsData: botsData
+    }))
+  }
+
+  addToBotArmy = (bot) =>{ 
+    this.setState({
+      toAddBot: bot
+    })
+  }
 
   render() {
+    //console.log(this.state.allBotsData)
     return (
       <div>
-        {/* put your components here */}
+        {<YourBotArmy incomingBot={this.state.toAddBot}/>}
+        {<BotCollection allBots={this.state.allBotsData} addToBotArmy={this.addToBotArmy}/>}
       </div>
     );
   }
-
 }
 
 export default BotsPage;
