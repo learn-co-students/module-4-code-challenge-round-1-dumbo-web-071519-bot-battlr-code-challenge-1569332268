fe137836905fbb26ec3578bfb090d0d7b5c2cc51
github cjl248 <github cjl248@cornell.edu>
2019-09-24 11:01:33 -0400

    hit checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,12 +4,17 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
+  renderBots = () => {
+    return this.props.bots.map((bot) => {
+      return <BotCard key={bot.id} bot={bot} enlistBot={this.props.enlistBot} />
+    })
+  }
+
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.renderBots()}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,14 +1,83 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state ={
+    bots: [],
+    freeBots: [],
+    userBots: []
+  }
+
+  enlistBot = (id) => {
+    const newBot = this.state.freeBots.find((bot) => {
+      return bot.id === id
+    })
+    // checking if the clicked bot is in the freeBots array
+    if (newBot !== undefined) {
+      this.setState({
+        userBots: [...this.state.userBots, newBot],
+        freeBots: this.state.freeBots.filter((bot) => {
+          return (bot.id !== id)
+        })
+      })
+    }
+    // else it has to be in the userBots array
+    else {
+      const botToDischarge = this.state.bots.find((bot) => {
+        return bot.id === id
+      })
+      console.log(botToDischarge)
+
+      this.setState({
+        freeBots: [...this.state.freeBots, botToDischarge],
+        userBots: this.state.userBots.filter((bot) => {
+          return bot.id !== botToDischarge.id
+        })
+      })
+    }
+
+    // const newUserBotList = this.state.bots.filter((bot) => {
+    //   return bot.id === id
+    // })
+
+    // this.setState({
+    //   userBots: this.state.bots.map((bot) => {
+    //     if(bot.id === id) return bot
+    //   })
+    // })
+
+    // const newBotList = this.state.bots.filter((bot) => {
+    //   return bot.id !== id
+    // })
+    // console.log(newBotList)
+
+
+  }
+
+  loadBots = (bots) => {
+    this.setState({
+      bots,
+      freeBots: bots
+    })
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy bots={this.state.userBots} enlistBot={this.enlistBot}/>
+        <BotCollection bots={this.state.freeBots} enlistBot={this.enlistBot} />
       </div>
     );
+  }
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(resp => resp.json())
+      .then(this.loadBots)
+
   }
 
 }
