95774d259ff5f5643d4de147e3172fcb40a81ac4
Janet Mendez <janetmendez.03@gmail.com>
2019-09-24 10:36:40 -0400

    got the show page to work, need to refactor

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BotCollection from "./BotCollection";
 import YourBotArmy from './YourBotArmy'
+import BotSpecs from "../components/BotSpecs";
 
 class BotsPage extends React.Component {
   state = {
     bots: [],
-    enlisted: []
+    enlisted: [],
+    show: false,
+    showingBot: []
   }
   onEnlist = (bot) => {
     console.log("I am enlisted!", bot)
@@ -25,11 +28,36 @@ class BotsPage extends React.Component {
 
   onDischarge = (bot) => {
     console.log("I am being discharged!", bot)
+    //! Filter out the bot that you wanna remove
     this.setState({
       enlisted: this.state.enlisted.filter(b => b !== bot)
     })
   }
 
+  changeToShow = (bot) => {
+    this.setState({
+      show: true,
+      showingBot: bot
+    })
+  }
+
+  onBack = () => {
+    this.setState({
+      show: false,
+      showingBot: []
+    })
+  }
+
+
+  renderView = () => {
+    if(this.state.show){
+      return <BotSpecs bot={this.state.showingBot} onEnlist={this.onEnlist} onBack={this.onBack}/>
+    }
+    else{
+      // <BotCollection bots={this.state.bots} callback={this.onEnlist}/>
+      return <BotCollection bots={this.state.bots} callback={this.changeToShow}/>
+    }
+  }
   componentDidMount(){
     fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
     .then(res => res.json())
@@ -45,7 +73,7 @@ class BotsPage extends React.Component {
       <div>
         {/* put your components here */}
         <YourBotArmy bots={this.state.enlisted} callback={this.onDischarge}/>
-        <BotCollection bots={this.state.bots} callback={this.onEnlist}/>
+        {this.renderView()}
       </div>
     );
   }
